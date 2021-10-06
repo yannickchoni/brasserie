b3ad3a17a0a42655590b2fdeb5340609cf5a4441
yannickchoni <yannickjulberts@gmail.com>
2021-10-06 10:11:50 +0200

    [N] Init project

---
+++ app/src/main/java/com/brasimba/livraison/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.brasserie.livraison
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ActivityMainBinding binding =
+        setContentView(getRo)
+    }
+}

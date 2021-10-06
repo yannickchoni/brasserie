8cab57f7863628b6389e3239c98dd8787d5ba061
yannickchoni <yannickjulberts@gmail.com>
2021-10-06 11:34:42 +0200

    [N] Add splash screen

--- app/src/main/java/com/brasserie/livraison/MainActivity.kt
+++ app/src/main/java/com/brasserie/livraison/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.brasserie.livraison
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.brasserie.livraison.databinding.ActivityMainBinding
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Thread{
+            Thread.sleep(3000)
+            Log.e("H","HELLO")
+        }.start()
     }
 }

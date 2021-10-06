1ab54dd03130d7ffdf796f936203963e86f6a19b
yannickchoni <yannickjulberts@gmail.com>
2021-10-06 16:26:56 +0200

    [N] Firebase authentication (only phone provider)

--- app/src/main/java/com/brasserie/livraison/MainActivity.kt
+++ app/src/main/java/com/brasserie/livraison/MainActivity.kt
@@ -4,15 +4,35 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.brasserie.livraison.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val auth = FirebaseAuth.getInstance()
+        val starterIntent = Intent(this,HomeActivity::class.java)
+        val signInLauncher = registerForActivityResult(FirebaseAuthUIActivityResultContract()) { result ->
+            if(result.resultCode == RESULT_OK && auth.currentUser != null)
+                startActivity(starterIntent)
+        }
+
         Thread{
             Thread.sleep(3000)
-            startActivity(Intent(this,HomeActivity::class.java))
+            if (auth.currentUser != null){
+                val authProviders = arrayListOf(AuthUI.IdpConfig.PhoneBuilder().build())
+                val signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(authProviders)
+                    .setLogo(R.drawable.logo_brasimba_rdcongo)
+                    .build()
+                signInLauncher.launch(signInIntent)
+            }
+            else
+                startActivity(starterIntent)
         }.start()
     }
 }

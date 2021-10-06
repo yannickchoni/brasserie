3ac91cfde03774a36b336fdc08f4e4699c9fd7c0
yannickchoni <yannickjulberts@gmail.com>
2021-10-06 10:55:33 +0200

    [N] Enable view binding

--- app/src/main/java/com/brasserie/livraison/MainActivity.kt
+++ app/src/main/java/com/brasserie/livraison/MainActivity.kt
@@ -2,11 +2,13 @@ package com.brasserie.livraison
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.brasserie.livraison.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ActivityMainBinding binding =
-        setContentView(getRo)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
